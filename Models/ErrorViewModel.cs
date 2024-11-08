7e86f564455a4aa735f3918f998b203db98d7cf0
“LucianoFalco” <lucianosebastianfalco@gmail.com>
2024-11-08 09:36:38 -0300

    inicio del proyecto

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,8 @@
+namespace tl2_tp6_2024_lucianofalco.Models;
+
+public class ErrorViewModel
+{
+    public string? RequestId { get; set; }
+
+    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+}
